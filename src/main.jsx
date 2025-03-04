332dfbb04bee37afd16a8e63be92f4d1e57225df
Bushan SC <bushan1992@zohomail.in>
2025-03-04 07:01:01 +0530

    added header

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,10 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
