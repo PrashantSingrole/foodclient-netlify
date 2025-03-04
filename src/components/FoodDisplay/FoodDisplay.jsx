4cc796905db410145328abea14daf8c071b6f22d
Bushan SC <bushan1992@zohomail.in>
2025-03-04 23:44:41 +0530

    completed filtering

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
+
+    const {foodList} = useContext(StoreContext);
+    const filteredFoods = foodList.filter(food => (
+        category === 'All' || food.category === category
+    ));
   return (
-    <div>FoodDisplay</div>
+    <div className="container">
+        <div className="row">
+            {filteredFoods.length > 0 ? (
+                filteredFoods.map((food, index) => (
+                    <FoodItem key={index} 
+                        name={food.name} 
+                        description={food.description}
+                        id={food.id}
+                        imageUrl={food.imageUrl}
+                        price={food.price} />
+                ))
+            ) : (
+                <div className="text-center mt-4">
+                    <h4>No food found.</h4>
+                </div>
+            )}
+        </div>
+    </div>
   )
 }
 
