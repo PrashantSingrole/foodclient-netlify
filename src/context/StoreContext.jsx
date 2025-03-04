99d2a3d2fa36f11f3821c11dfec03b3a09ff1683
Bushan SC <bushan1992@zohomail.in>
2025-03-04 12:06:57 +0530

    food menu completed

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const StoreContext = createContext(null);
+
+export const StoreContextProvider = (props) => {
+
+    const [foodList, setFoodList] = useState([]);
+
+    const fetchFoodList = async () => {
+        const response = await axios.get('http://localhost:8080/api/foods');
+        setFoodList(response.data);
+        console.log('food data from api', response.data);
+        
+    }
+
+    const contextValue = {
+        foodList
+    };
+
+    useEffect(() => {
+        async function loadData() {
+            await fetchFoodList();
+        }
+        loadData();
+    }, []);
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
